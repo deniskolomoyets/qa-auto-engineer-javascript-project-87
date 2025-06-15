3b73d7922fe6e4bdde34c88307c6aa081aa49e72
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2025-06-15 13:05:37 +0300

    feat: add argument parsing and format option to CLI

--- gendiff.js
+++ gendiff.js
@@ -4,7 +4,15 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    console.log(`File 1: ${filepath1}`);
+    console.log(`File 2: ${filepath2}`);
+    console.log(`Format: ${options.format}`);
+  });
 
 program.parse();

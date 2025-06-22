20214d567ead7904ec150250e908afbc2116e94f
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2025-06-22 12:21:32 +0300

    fix: eslint config and autofix

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,21 @@
 import globals from 'globals';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 import importPlugin from 'eslint-plugin-import';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-
-});
 
 export default [
   {
     languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
     },
-    plugins: { import: importPlugin },
+    plugins: {
+      import: importPlugin,
+    },
     rules: {
       ...importPlugin.configs.recommended.rules,
-    },
-  },
-  ...compat.extends('airbnb-base'),
-  {
-    rules: {
       'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
       'import/extensions': ['error', { js: 'always' }],
       'import/no-named-as-default': 'off',
@@ -39,5 +23,8 @@ export default [
       'no-console': 'off',
       'import/no-extraneous-dependencies': 'off',
     },
+  },
+  {
+    ignores: ['node_modules/', 'dist/'],
   },
 ];

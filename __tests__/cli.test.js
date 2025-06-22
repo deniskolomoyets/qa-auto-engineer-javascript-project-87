d6015ecd90badafd89c4341521523580eda0f2da
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2025-06-22 13:22:29 +0300

    added tests for coverage

--- __tests__/cli.test.js
+++ __tests__/cli.test.js
@@ -1,5 +1,6 @@
 import { execSync } from 'node:child_process'
 import { fileURLToPath } from 'url'
+import { readFileSync } from 'fs'
 import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -15,4 +16,24 @@ test('gendiff CLI: json output', () => {
   const result = execSync(command)
 
   expect(result.toString()).toContain('{')
+})
+
+test('gendiff CLI: stylish output', () => {
+  const filepath1 = getFixturePath('file1.json')
+  const filepath2 = getFixturePath('file2.json')
+  const command = `node ./gendiff.js "${filepath1}" "${filepath2}" --format stylish`
+  const result = execSync(command).toString()
+
+  const expected = readFileSync(getFixturePath('result.txt'), 'utf-8').trim()
+  expect(result.trim()).toBe(expected)
+})
+
+test('gendiff CLI: plain output', () => {
+  const filepath1 = getFixturePath('file1.json')
+  const filepath2 = getFixturePath('file2.json')
+  const command = `node ./gendiff.js "${filepath1}" "${filepath2}" --format plain`
+  const result = execSync(command).toString()
+
+  const expected = readFileSync(getFixturePath('plain-result.txt'), 'utf-8').trim()
+  expect(result.trim()).toBe(expected)
 })
